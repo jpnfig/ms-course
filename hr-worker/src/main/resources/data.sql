1a60554bd596388d3d8e5fc93c0aa67b3de3c6a6
João Paulo Nóbrega Figueiredo <joaopnobrega@gmail.com>
2021-03-29 18:58:04 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Bob', 200.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Mary', 300.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Ale', 250.0);
